cc861be6534f43dcf8ae007076dfd4ea20c3bb76
Tim Ubial <tim.ubial@gmail.com>
2025-10-02 11:19:17 -0700

    Added CYOA to repository

---
+++ activity-cyoa-Steve Jobs.py
@@ -0,0 +1,32 @@
+# Choose Your Own Adventure
+# Steve Jobs
+# 24 September 2025
+
+# Choose your own adventure story focusing on
+# using variables and branching/conditionals
+
+from os import pathconf_names
+import time
+
+# Introduction
+
+# Describe the setting
+print("You're standing in front of a brown house.")
+time.sleep(1.5)
+print("The door at the front is open.")
+time.sleep(1.5)
+print("There's a gate on the left side of the house.")
+time.sleep(1.5)
+
+path_choice = input("Do you go in the door or in the gate? (answer door or gate)")
+
+if path_choice == "gate":
+    print("You go into the gate.")
+
+# Problem
+
+# Rising Action
+
+# Climax
+
+# Resolution
