5958e5287564a96478f9c88386eeb4c4790d6fe5
Tim Ubial <tim.ubial@gmail.com>
2025-11-05 13:05:18 -0800

    added numbers notes

---
+++ notes-6-numbers.py
@@ -0,0 +1,23 @@
+# Numbers and Operations
+# Author: Ubial
+# 5 November 2025
+
+# Create an algorithm to gather
+# data to find the most popular
+# bubble tea place around us
+
+# Version 1
+# Show all the bbt choices
+# Ask the user for their choice
+# Add their vote to a running
+# tally
+# Give some raw scores
+# Give score as a percentage
+
+# Version 2
+# Ask the user what their fave
+# bbt place is
+# Add their vote to a running
+# tally
+# Give the raw score
+# Give score as percentage
