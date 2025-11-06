fb6d6b5b547a6672a7bf3a9cb44c0a856abd0a26
Tim Ubial <tim.ubial@gmail.com>
2025-11-06 12:43:42 -0800

    numbers notes update

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -7,12 +7,27 @@
 # bubble tea place around us
 
 # Version 1
-# Show all the bbt choices
-# Ask the user for their choice
-# Add their vote to a running
-# tally
-# Give some raw scores
-# Give score as a percentage
+def vote_listed_choices():
+    """Display all voting choices
+    5 users vote for their choice
+    Results will be printed"""
+    CHOICES = [
+        "A. CoCo",
+        "B. Chatime",
+        "C. BUBBLE WAFFEL",
+        "D. gong cha"
+    ]
+
+    # Show all the bbt choices
+    print("Vote for your favourite from the list.")
+    print("Give the letter of your choice.")
+    for choice in CHOICES:
+        print(choice)
+    # Ask the user for their choice
+    # Add their vote to a running
+    # tally
+    # Give some raw scores
+    # Give score as a percentage
 
 # Version 2
 # Ask the user what their fave
@@ -21,3 +36,9 @@
 # tally
 # Give the raw score
 # Give score as percentage
+
+def main():
+    vote_listed_choices()
+
+if __name__ == "__main__":
+    main()
