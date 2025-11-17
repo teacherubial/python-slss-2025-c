1fdbe564df311c5b94d3c28a62bb258d132e83c5
Tim Ubial <tim.ubial@gmail.com>
2025-11-17 08:56:06 -0800

    numbers notes updated

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -6,6 +6,10 @@
 # data to find the most popular
 # bubble tea place around us
 
+import os
+
+NUM_VOTERS = 100
+
 # Version 1
 def vote_listed_choices():
     """Display all voting choices
@@ -25,32 +29,35 @@ def vote_listed_choices():
     gong_cha = 0
     spoiled_votes = 0
 
-    # Show all the bbt choices
-    print("Vote for your favourite from the list.")
-    print("Give the letter of your choice.")
-    for choice in CHOICES:
-        print(choice)
-    # Ask the user for their choice
-    vote = input("Your vote: ").lower().strip(",.?! ")
-    # Add their vote to a running
-    # tally
-    if vote == "a":
-        coco = coco + 1  # incrementation
-    elif vote == "b":
-        chatime += 1
-    elif vote == "c":
-        bubble_waffel += 1
-    elif vote == "d":
-        gong_cha += 1
-    else:
-        spoiled_votes += 1
-    # Give some raw scores
+    for _ in range(NUM_VOTERS):
+        # Clear the screen for the voter
+        os.system("clear")
+        print("Vote for your favourite from the list.")
+        print("Give the letter of your choice.")
+        for choice in CHOICES:
+            print(choice)
+
+        vote = input("Your vote: ").lower().strip(",.?! ")
+
+        # Add their vote to a running tally
+        if vote == "a":
+            coco += 1
+        elif vote == "b":
+            chatime += 1
+        elif vote == "c":
+            bubble_waffel += 1
+        elif vote == "d":
+            gong_cha += 1
+        else:
+            spoiled_votes += 1
+
     # Show the scores of coco, ..., etc.
     print(f"CoCo votes: {coco}")
     print(f"Chatime votes: {chatime}")
     print(f"BUBBLE WAFFEL votes: {bubble_waffel}")
     print(f"Gong Cha votes: {gong_cha}")
     print(f"Spoiled votes: {spoiled_votes}")
+
     # Give score as a percentage
     coco_percentage = coco / (coco + chatime + bubble_waffel + gong_cha + spoiled_votes)
     print(f"CoCo Percentage: {coco_percentage * 100}%")
@@ -69,8 +76,44 @@ def vote_listed_choices():
 # Give the raw score
 # Give score as percentage
 
+def chip_rater():
+    """Help gather data about chip crispness
+    and quality."""
+    # Create a list of questions
+    questions = [
+        "How crispy is the chip out of 5? 0 is mushy, 5 is super crisp.",
+        "How would you rate the taste out of 5? 0 is unpalatable, 5 is the most delicious thing you've ever eaten.",
+        "How fresh would you rate the chip out of 5? 0 is stale, 5 is super fresh.",
+        "How would you rate the size of the chip? 0 is terrible, 5 is perfect."
+    ]
+
+    # Rating total
+    total = 0
+
+    # Give user instructions
+    print("Take one chip from the bag.")
+    print("Eat it mindfully.")
+    print("Give your rating.")
+
+    # Ask the user questions
+    for question in questions:
+        print(question)
+        # For each question, get their rating
+        # out of five
+        rating = int(input("Your rating: ").strip(",.?! "))
+
+        # Update total
+        total += rating
+
+    # Print the average rating out of five
+    average = total / len(questions)
+
+    print(f"The average rating for this chip is: {average}.")
+
+
 def main():
-    vote_listed_choices()
+    # vote_listed_choices()
+    chip_rater()
 
 if __name__ == "__main__":
     main()
